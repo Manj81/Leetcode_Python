9dc1069ad4a3770b1cbbe81c5e2315725045a2fb
Manjeera Bonumahanti <87996412+Manj81@users.noreply.github.com>
2023-06-28 13:37:06 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,5 @@
 print("::error::Hello World Error")
+print("::error::Hello World Error2")
 #print("debug Hello World Debug")
 print("::warning:: Hello World Warning")
 print("::notice:: Hello World Notice")
